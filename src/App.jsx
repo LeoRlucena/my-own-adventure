5dbcbc9a7ea73c19c23b2a68fe682fbeeae2d2ff
Leonardo Lucena <125993424+LeoRlucena@users.noreply.github.com>
2025-01-23 13:26:32 +0000

    updating siteProps with my own data

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Leonardo Lucena",
+  title: "System Development Student",
+  email: "leorlucena@gmail.com",
+  gitHub: "https://github.com/LeoRlucena",
+  instagram: "https://www.instagram.com/leorlucena/",
+  linkedIn: "https://www.linkedin.com/in/leo-lucena24/",
+  youTube: "https://www.youtube.com/@LeoRlucena",
 };
 
 const primaryColor = "#4E567E";
